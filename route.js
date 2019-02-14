288430a3b329acfa0cf2f76c917cc3eeeda85354
Jinjinism <idmania153@naver.com>
2019-02-15 04:12:11 +0900

    test5

--- route.js
+++ route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = express.Router(); // 라우터 분리
 router.get('/', (req, res) => { //app 대신 router에 연결
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+  res.sendFile(path.join(__dirname, 'html', 'example.html'));
 });
 router.get('/1', (req, res) => {
   res.sendFile(path.join(__dirname, 'html', '1.html'));
