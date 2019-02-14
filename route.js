c5e2c63168af571f69637b2850fefb65c780764c
Jinjinism <idmania153@naver.com>
2019-02-15 00:31:04 +0900

    test4

---
+++ route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router(); // 라우터 분리
+router.get('/', (req, res) => { //app 대신 router에 연결
+  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+});
+router.get('/1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', '1.html'));
+});
+module.exports = router; //모듈로 만드는 부분
+
+//코드를 분리했다는 것에 의의
