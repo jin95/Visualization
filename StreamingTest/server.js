6934edcc264858fb7649dd787951628bbf5e0052
Sung Wung, Yeom <yeom4032yeom4032@gmail.com>
2019-02-18 23:17:19 +0900

    Create server.js

---
+++ StreamingTest/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var RTSP = require('./RTSP/rtsp.js');
+var RTMP = require('./RTMP/rtmp.js');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
