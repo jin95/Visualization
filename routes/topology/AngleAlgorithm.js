9216d5638eae15af9f8f523f2a4ad91642b87ee6
Jinjinism <idmania153@naver.com>
2019-02-24 20:51:17 +0900

    Algorithm

--- routes/topology/AngleAlgorithm.js
+++ routes/topology/AngleAlgorithm.js
@@ -1,22 +1,23 @@
+var AngleAlgorithm = function(n , x , y){
+    //a, b 센터좌표
+    //반복문
+    var angle = 360/n /180 * Math.PI * i;  //Degree를 Radian으로 변경
+    //i번 회전 ~n까지
 
+    var cos = Math.cos(angle);
+    var sin = Math.sin(angle);
 
-
-
-n번 반복
-
-//rotate
-function(var n,var x1,var y1) {
-
-  var Angle = 360/ n / 180 * Math.PI; //Degree를 Radian으로 변경
-
-  var cos = Math.cos(Angle);
-  var sin =Math.sin(Angle);
-  // a,b 센터좌표
-  // x2 y2 회전 후 좌표
-  var x2 = (x1-a) * cos - (y1-b)sin + a;
-  var y2 = (x1-a) * sin + (y1-b)cos + b;
+    var ax = (x-a) * cos - (y-b) * sin + a;
+    var ay = (x-a) * sin + (y-b) * cos + b;
+    // a,b 센터좌표
+    //ax , ay 값이 회전 후 좌표
 
 }
 
 
 //
+nodesTable.addRow([3, "지역2", (575-425) * (Math.cos(360/3/180*Math.PI)) -(300-300) * Math.sin(360/3/180*Math.PI)+ 425,
+                   (575-425)* Math.sin(360/3/180*Math.PI) + (300-300)* Math.cos(360/3/180*Math.PI)+ 300])
+
+nodesTable.addRow([4, "지역3", (575-425) * (Math.cos(360/3/180*Math.PI*2)) -(300-300) * Math.sin(360/3/180*Math.PI*2)+ 425,
+                   (575-425)* Math.sin(360/3/180*Math.PI*2) + (300-300)* Math.cos(360/3/180*Math.PI*2)+ 300])
