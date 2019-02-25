437511f14f69780d878570fa529b088c24418216
Jinjinism <idmania153@naver.com>
2019-02-25 17:00:23 +0900

    update

--- routes/topology/net.js
+++ routes/topology/net.js
@@ -33,11 +33,11 @@ google.setOnLoadCallback(drawVisualization);
 
        // Case Node5.  .....
 
-       nodesTable.addRow([2, "지역1",575,300]);
-       nodesTable.addRow([3, "지역2", (575-425) * (Math.cos(360/3/180*Math.PI)) -(300-300) * Math.sin(360/3/180*Math.PI)+ 425,
+      nodesTable.addRow([2, "지역1",575,300]);
+      nodesTable.addRow([3, "지역2", (575-425) * (Math.cos(360/3/180*Math.PI)) -(300-300) * Math.sin(360/3/180*Math.PI)+ 425,
                           (575-425)* Math.sin(360/3/180*Math.PI) + (300-300)* Math.cos(360/3/180*Math.PI)+ 300])
 
-     nodesTable.addRow([4, "지역3", (575-425) * (Math.cos(360/3/180*Math.PI*2)) -(300-300) * Math.sin(360/3/180*Math.PI*2)+ 425,
+      nodesTable.addRow([4, "지역3", (575-425) * (Math.cos(360/3/180*Math.PI*2)) -(300-300) * Math.sin(360/3/180*Math.PI*2)+ 425,
                           (575-425)* Math.sin(360/3/180*Math.PI*2) + (300-300)* Math.cos(360/3/180*Math.PI*2)+ 300])
 /*
        nodesTable.addRow([3, "Arduino", "circle",625,300]);
@@ -86,3 +86,38 @@ google.setOnLoadCallback(drawVisualization);
        // Draw our network with the created data and options
        network.draw(nodesTable, linksTable, options);
      }
+
+
+//AngleAlgorithm
+// 지역 추가 1개 - 고정 좌표 값
+// 지역 추가 2개 - 알고리즘 적용
+// 지역 추가 3개 - 알고리즘 적용
+// 지역 추가 4개 - 알고리즘 적용하면서 각도 조금씩 이동.
+// 지역 추가 5개 - 알고리즘 적용
+// n지역 추가 카운트 , (x,y) 첫번째 점의 좌표
+     function AngleAlgorithm(n , x , y){
+         //반복문
+         var i = 1;
+
+         if( n == 1 ){ //첫 지역 추가는 고정위치
+           ax = x + ?;
+           ay = y;
+         }
+         else{
+           while(i < n){
+             //i번 회전 ~n까지
+             var angle = 360/n /180 * Math.PI * i;  //Degree를 Radian으로 변경
+             var cos = Math.cos(angle);
+             var sin = Math.sin(angle);
+             var ax = (x-a) * cos - (y-b) * sin + a; // a,b 센터좌표
+             var ay = (x-a) * sin + (y-b) * cos + b; //ax , ay 값이 회전 후 좌표
+             i = i+1;
+           }
+         }
+     }
+     //
+     // nodesTable.addRow([3, "지역2", (575-425) * (Math.cos(360/3/180*Math.PI)) -(300-300) * Math.sin(360/3/180*Math.PI)+ 425,
+     //                    (575-425)* Math.sin(360/3/180*Math.PI) + (300-300)* Math.cos(360/3/180*Math.PI)+ 300])
+     //
+     // nodesTable.addRow([4, "지역3", (575-425) * (Math.cos(360/3/180*Math.PI*2)) -(300-300) * Math.sin(360/3/180*Math.PI*2)+ 425,
+     //                    (575-425)* Math.sin(360/3/180*Math.PI*2) + (300-300)* Math.cos(360/3/180*Math.PI*2)+ 300])
